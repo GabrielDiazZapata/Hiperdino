b8a49d7e0c9bf00085e347dc30372c9cacf9bf90
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-05-04 13:00:35 +0100

    feat(GeneradorProductos.java): Generador automatico de productos creado.

--- src/com/hiperdino/generadores/GeneradorProductos.java
+++ src/com/hiperdino/generadores/GeneradorProductos.java
@@ -1,5 +1,10 @@
 package com.hiperdino.generadores;
-
+import java.util.Random;
 public class GeneradorProductos {
+    private static String[] productos = {"Leche", "Huevos", "Pan", "Azúcar", "Sal", "Aceite", "Café", "Té", "Arroz", "Pasta", "Carne", "Pescado"};
     
+    public static String generadorProductos(){
+        Random rand = new Random();
+        return productos[rand.nextInt(productos.length)];
+    }
 }
