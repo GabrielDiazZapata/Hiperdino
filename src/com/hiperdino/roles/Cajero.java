85a2734ed2a58a47f6895ac8ec0875c56dd611ca
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-05-04 18:47:23 +0100

    Fix: Correccion de varios errores.

--- src/com/hiperdino/roles/Cajero.java
+++ src/com/hiperdino/roles/Cajero.java
@@ -1,9 +1,10 @@
-package com.hiperdino.roles;
+package roles;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Cajero {
     private int numeroCaja;
+    private boolean cajaAbierta;
     private Queue<Cliente> filaClientes;
 
     public Cajero(int numeroCaja) {
@@ -29,6 +30,18 @@ public class Cajero {
 
     public boolean hayClientesEnFila() {
         return !filaClientes.isEmpty();
+    }
+
+    public void abrirCaja() {
+        this.cajaAbierta = true;
+    }
+
+    public Queue<Cliente> getFilaClientes() {
+        return filaClientes;
+    }
+
+    public void cerrarCaja() {
+        this.cajaAbierta = false;
     }
 
     public void mostrarFilaClientes() {
